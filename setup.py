1b21075cd813fc9e857f55ba337e4031de927e68
Przemek Bubas <88086300+pbubas@users.noreply.github.com>
2022-05-29 21:31:04 +0200

    Pypi update (#6)
    * modified version to upload to pypi
    
    * new release to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gotify_message",
-    version="0.1.0",
+    version="0.1.1",
     author="Przemek Bubasr",
     author_email="bubasenator@gmail.com",
     description="Python module to push messages to gotify server",
